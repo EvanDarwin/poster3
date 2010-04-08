ac1c3d46de640245fb88774384355990bf1f13ea
Chris AtLee <chris@atlee.ca>
2010-04-08 08:09:10 -0400

    adding note about which files to edit when bumping version

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 from distutils.cmd import Command
 import poster
 
+# To update version number, edit:
+# poster/__init__.py
+# docs/index.rst
 version = ".".join(str(x) for x in poster.version)
 
 class sphinx_command(Command):
